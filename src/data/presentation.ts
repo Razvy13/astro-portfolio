148b996a9de3e560dfeb1ce33b71dd88aa58f3bc
Razvan Dimulescu <razvan.dimulescu@gmail.com>
2023-08-09 23:36:39 +0200

    initial commit

---
+++ src/data/presentation.ts
@@ -0,0 +1,34 @@
+type Social = {
+  label: string;
+  link: string;
+};
+
+type Presentation = {
+  mail: string;
+  title: string;
+  description: string;
+  socials: Social[];
+};
+
+const presentation: Presentation = {
+  mail: "razvan.dimulescu@gmail.com",
+  title: "Hi, I’m Raz 👋",
+  description:
+    "My name is *Razvan Dimulescu* and I'm a *Frontend Developer*. I'm from Romania, living in Italy and currently working with *Angular* and *TaigaUI* at Aton Spa. Outside of work you can find me on *mountain*, snowboarding, hiking or climbing.",
+  socials: [
+    {
+      label: "X",
+      link: "https://twitter.com/razvan_dim",
+    },
+    {
+      label: "Instagram",
+      link: "https://www.instagram.com/razvan.dimulescu",
+    },
+    {
+      label: "Github",
+      link: "https://github.com/Razvy13",
+    },
+  ],
+};
+
+export default presentation;
