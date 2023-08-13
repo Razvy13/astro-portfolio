e9086111fa09f976b6b6b9d7352792f479a2b5e4
Razvan Dimulescu <razvan.dimulescu@gmail.com>
2023-08-13 22:52:35 +0200

    Deploy configurations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 import { SITE_URL } from "./src/data/config";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap()],
   site: SITE_URL,
+  base: "/astro-portfolio",
   markdown: {
     syntaxHighlight: "shiki",
     shikiConfig: {
